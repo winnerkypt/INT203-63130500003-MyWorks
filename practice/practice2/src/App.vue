87f484b418f3d99d77fdf236f53236dfed64bbbd
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2022-02-07 20:14:57 +0700

    finished but no more efficiency

--- practice/practice2/src/App.vue
+++ practice/practice2/src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref} from 'vue';
 const lists = ref()
 const filter = ref()
-
-
 function search(){
- 
   const w = document.getElementsByTagName("li")
-   
   for (let index = 0; index < w.length; index++) {
-
     let y = w[index].textContent
     if (y.indexOf(filter.value) > -1) {
       w[index].style.display = ''
@@ -20,7 +15,6 @@ function search(){
 }
 function addNote() {
   if (lists.value != null && lists.value != "") {
-    // document.getElementById("list").insertAdjacentHTML("beforeend", "<li>" + lists.value + "</li>")
     const a = document.getElementById("list")
     const b = document.createElement("li")
     const c = document.createTextNode(lists.value)
@@ -28,11 +22,6 @@ function addNote() {
     b.appendChild(c)
   }
 }
-// const search = computed(() => 
-
-
-
-
 </script>
  
 <template>
@@ -41,13 +30,11 @@ function addNote() {
     <input type="text" id="filter" v-model="filter" @keyup="search"/>
     <ul id="list"></ul>
   </div>
-
   <p>
     New Note :
     <input type="text" v-model="lists" />
     <button @click="addNote">Add Note</button>
   </p>
 </template>
- 
 <style>
 </style>
