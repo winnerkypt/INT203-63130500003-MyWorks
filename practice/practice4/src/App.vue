2c5182e3d893e0784b0d1d130a6a3271b9d3c83a
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2022-04-04 21:54:56 +0700

    [finish] basic components

---
+++ practice/practice4/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import List from './components/List.vue'
+const infoProj = [
+  { 
+    'GitRepo': 'umaporn-sup/INT203class',
+    'ProjectTitle':'Bingo Game'
+  },
+  {
+    'GitRepo': 'students/Sec2_Group1',
+    'ProjectTitle':'Flash Card'
+  },
+  { 
+    'GitRepo': 'students/Sec3_Group2',
+    'ProjectTitle':'Tic Tac Toe'
+  }
+]
+
+const newMsg = ref('');
+const showConfirmMsg = (confirmMsg) =>{
+  newMsg.value=confirmMsg
+}
+</script>
+ 
+<template>
+<div>
+  <h1>INT203' student projects</h1>
+  <List :lists="infoProj" @confirm="showConfirmMsg"/>
+  <p>current status: {{ newMsg }}</p>
+</div>
+</template>
+ 
+<style>
+
+</style>
---
+++ practice/practice4/src/components/List.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineEmits(['confirm'])
+defineProps({
+    lists: {
+        type: Object,
+        require: true
+    }   
+})
+const msg= 'user_confirm'
+</script>
+ 
+<template>
+    <div>
+        <ul>
+            <li v-for="(list, index) in lists" :key="index">
+                Git Repo: {{ list.GitRepo }}
+                <br />
+                Project Title: {{ list.ProjectTitle }}
+            </li>
+        </ul>
+        <button @click="$emit('confirm',msg.toUpperCase())">Confirm</button>
+    </div>
+</template>
+ 
+<style>
+body {
+    line-height: 2;
+}
+</style>
